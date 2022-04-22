00d59b795b4ea4979d90d6063adf68c296b61207
周桑炜 <568356727@qq.com>
2022-04-22 14:28:46 +0800

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+
+const Home = () => import(/* webpackChunkName: "Home" */ '@/views/Home.vue');
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,15 +11,12 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import('../views/About.vue'),
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
